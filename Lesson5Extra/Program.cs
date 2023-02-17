50347b13a07e8ac2303e307f231b816cbf5fd89e
gorbacheva.diana.alekseevna <dexpvallfreya@gmail.com>
2023-02-17 13:08:37 +0300

    delete comments

--- Lesson5Extra/Program.cs
+++ Lesson5Extra/Program.cs
@@ -1,6 +1,4 @@
-﻿using Lesson5Extra.Enums;
-using Lesson5Extra.Models;
-using Lesson5Extra.Repositories;
+﻿using Lesson5Extra.Repositories;
 
 public class Program
 {
@@ -9,16 +7,6 @@ public class Program
         var repository = new CarRepository();
         var cars = repository.GetCars();
         var changedCars = repository.GetChangedCars();
-        // если год обновления машины текущий, ничего не делать, 
-        // если год не текущий, считаем разницу между текущим и последним годом и за каждый год прибавляем 10_000 км пробега
-        // найти все машины которые мы изменили и вывести марку и модель машины, у которой мы обновили пробег в консоль
-
-        //var t = cars.Where(x => x.UpdateAt.Year < DateTime.Now.Year).Single();
-        //var r = DateTime.Now.Year - t.UpdateAt.Year;
-        //r = r * 10_000;
-        //t.Mileage = r + t.Mileage;
-        //Console.WriteLine($"Марка машины: {t.Brand} Модель машины: {t.Model} Пробег: {t.Mileage}км ");
-
         var result = cars.Where(x => x.CompareTo(changedCars.Where(y => y.Vin == x.Vin).Single()) > 0).ToList();
         foreach (var item in result)
         {
